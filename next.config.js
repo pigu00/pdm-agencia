d82a39f2ff91f30113ac7320cf3483d5b3f7cf18
Your NameRoberto <robertomarban@gmail.com>
2024-03-27 20:12:46 -0300

    d

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 /**
  * @type {import('next').NextConfig}
+ * 
  */
 const nextConfig = {
   output: 'export',
- 
+  
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
+   trailingSlash: true,
  
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
+  skipTrailingSlashRedirect: true,
   // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  distDir: 'dist',
 }
  
 module.exports = nextConfig
