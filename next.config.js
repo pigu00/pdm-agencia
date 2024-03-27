4a16d5673e1fc944a3f5c45426d16a9ce7652e20
Your NameRoberto <robertomarban@gmail.com>
2024-03-27 20:12:49 -0300

    d

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
    trailingSlash: true,
  
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  skipTrailingSlashRedirect: true,
+ // skipTrailingSlashRedirect: true,
   // Optional: Change the output directory `out` -> `dist`
   distDir: 'dist',
 }
